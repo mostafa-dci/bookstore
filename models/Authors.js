6b979bfdffa4f92b3f64fa6b47b097623891ee24
Mostafa Othman <mostafa.othman@digitalcareerinstitute.org>
2022-08-30 16:11:32 +0200

    add_author_schema

--- models/Authors.js
+++ models/Authors.js
@@ -8,3 +8,43 @@
 //     },
 //     "phone": String NOT required, min 10
 // }
+
+const mongoose = require('mongoose')
+
+const authorsSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email is already Exist"],
+        validate: [val=>{
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)) {
+                return true;
+                }
+                return false;
+        }, "is Not a valid"]
+    },
+    address: {
+        type: {
+            country: {
+                type: String,
+                required: true
+            },
+            city: {
+                type: String,
+                required: true
+            }
+        }
+    }, 
+    phone: {
+        type: String,
+        required: false
+    }
+},{collection: 'authors'})
+
+const AuthorsModel = mongoose.model("Authors", authorsSchema)
+
+module.exports = authorsSchema
